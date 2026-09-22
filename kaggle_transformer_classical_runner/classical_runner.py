bcaabbcb423a2995b81860aea9cba1e7e97c235e
swayamjeetbhaga <bhagatswayamjeet6@gmail.com>
2026-09-22 19:27:02 +0530

    Pin Transformer comparison jobs to screened code revision

--- kaggle_transformer_classical_runner/classical_runner.py
+++ kaggle_transformer_classical_runner/classical_runner.py
@@ -5,7 +5,7 @@ import subprocess
 import sys
 
 REPO = "https://github.com/GaneshJamadar369/Quantum-ML-hybrid.git"
-REVISION = "REVISION_TO_PIN"
+REVISION = "5202e07"
 
 
 def run(command, cwd=None):
